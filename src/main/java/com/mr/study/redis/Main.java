5516922f8193f98c45f3fb263c2b0065073c6b02
zhanxupeng <1098481123@qq.com>
2019-10-13 12:00:42 +0800

    Default Changelist

---
+++ src/main/java/com/mr/study/redis/Main.java
@@ -0,0 +1,12 @@
+package com.mr.study.redis;
+
+/**
+ * @author zhanxp
+ * @version 1.0  2019/10/13
+ * @Description todo
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
